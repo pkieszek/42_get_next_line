b8f145d03504974292c70dfa450c0de0834e5bb2
Patryk <fierte.06.signaler@icloud.com>
2025-02-19 16:22:37 +0100

    new changes applied

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,82 +6,83 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 02:34:01 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/19 15:22:59 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/19 16:22:01 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/* Returns the length of a string */
-size_t	ft_strlen(const char *s)
+size_t ft_strlen(const char *s)
 {
-	size_t	len;
-
-	len = 0;
-	while (s && s[len])
-		len++;
-	return (len);
+    size_t len = 0;
+    while (s && s[len])
+        len++;
+    return (len);
 }
 
-/* Finds the first occurrence of a character in a string */
-char	*ft_strchr(const char *s, int c)
+char *ft_strchr(const char *s, int c)
 {
-	if (!s)
-		return (NULL);
-	while (*s)
-	{
-		if (*s == (char)c)
-			return ((char *)s);
-		s++;
-	}
-	return (NULL);
+    if (!s)
+        return (NULL);
+    while (*s)
+    {
+        if (*s == (char)c)
+            return ((char *)s);
+        s++;
+    }
+    return (NULL);
 }
 
-/* Concatenates two strings and frees the first */
-char	*ft_strjoin(char *s1, char *s2)
+char *ft_strjoin(char *s1, char *s2)
 {
-	size_t	len1;
-	size_t	len2;
-	char	*new_str;
-	size_t	i;
-	size_t	j;
+    size_t len1 = ft_strlen(s1);
+    size_t len2 = ft_strlen(s2);
+    char *new_str = (char *)malloc(len1 + len2 + 1);
+    size_t i = 0, j = 0;
 
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	new_str = (char *)malloc(len1 + len2 + 1);
-	if (!new_str)
-		return (NULL);
-	i = 0;
-	j = 0;
-	if (s1)
-	{
-		while (s1[i])
-			new_str[i] = s1[i++];
-		free(s1);
-	}
-	while (s2[j])
-		new_str[i++] = s2[j++];
-	new_str[i] = '\0';
-	return (new_str);
+    if (!new_str)
+        return (NULL);
+    if (s1)
+    {
+        while (s1[i])
+            new_str[i] = s1[i++];
+        free(s1);
+    }
+    while (s2[j])
+        new_str[i++] = s2[j++];
+    new_str[i] = '\0';
+    return (new_str);
 }
 
-/* Duplicates a string */
-char	*ft_strdup(const char *s)
+char *ft_strdup(const char *s)
 {
-	size_t	len;
-	char	*dup;
-	size_t	i;
+    size_t len = ft_strlen(s);
+    char *dup = (char *)malloc(len + 1);
+    size_t i = 0;
 
-	len = ft_strlen(s);
-	dup = (char *)malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	i = 0;
-	while (s[i])
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+    if (!dup)
+        return (NULL);
+    while (s[i])
+    {
+        dup[i] = s[i];
+        i++;
+    }
+    dup[i] = '\0';
+    return (dup);
+}
+
+size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+    size_t i = 0;
+
+    if (!dst || !src)
+        return (0);
+    while (i + 1 < dstsize && src[i])
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    if (dstsize > 0)
+        dst[i] = '\0';
+    return (ft_strlen(src));
 }
