c06fe48513fd501aab1cc0eb5c0324bef69be0ac
Patryk <fierte.06.signaler@icloud.com>
2025-02-19 16:36:37 +0100

    fixes to prevent memory leakage and infinite loops

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,23 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 02:33:42 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/19 16:20:37 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/19 16:35:50 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+/*
+ * Reads from the file descriptor and appends the data to the buffer.
+ * It ensures that data is continuously read until a newline is encountered.
+ */
 static char *read_and_store(int fd, char *buffer)
 {
     char temp_buffer[BUFFER_SIZE + 1];
     ssize_t bytes_read;
 
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+        return (NULL);
     bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
     if (bytes_read <= 0)
         return (NULL);
@@ -24,11 +30,17 @@ static char *read_and_store(int fd, char *buffer)
     return (ft_strjoin(buffer, temp_buffer));
 }
 
+/*
+ * Extracts a single line from the buffer.
+ * The function allocates memory for the line and copies characters up to the newline.
+ */
 static char *extract_line(char *buffer)
 {
     char *line;
     size_t len = 0;
 
+    if (!buffer || buffer[0] == '\0')
+        return (NULL);
     while (buffer[len] && buffer[len] != '\n')
         len++;
     line = (char *)malloc(len + 2);
@@ -38,11 +50,17 @@ static char *extract_line(char *buffer)
     return (line);
 }
 
+/*
+ * Trims the buffer by removing the extracted line.
+ * It moves the remaining part of the buffer to the start.
+ */
 static char *trim_buffer(char *buffer)
 {
     char *new_buffer;
     char *newline_pos;
 
+    if (!buffer)
+        return (NULL);
     newline_pos = ft_strchr(buffer, '\n');
     if (!newline_pos)
     {
@@ -54,12 +72,16 @@ static char *trim_buffer(char *buffer)
     return (new_buffer);
 }
 
+/*
+ * The main function to retrieve the next line from the file descriptor.
+ * It maintains a static buffer to store the remaining data for the next call.
+ */
 char *get_next_line(int fd)
 {
     static char *buffer;
     char *line;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
+    if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
         return (NULL);
     buffer = read_and_store(fd, buffer);
     if (!buffer)
