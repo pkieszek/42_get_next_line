c517bbde40778823a174aa3b24cb8830a5187798
Patryk <fierte.06.signaler@icloud.com>
2025-02-19 02:40:21 +0100

    header defined

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,24 @@
 /*   By: pkieszek <pkieszek@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 02:34:13 by pkieszek          #+#    #+#             */
-/*   Updated: 2025/02/19 02:34:14 by pkieszek         ###   ########.fr       */
+/*   Updated: 2025/02/19 02:39:31 by pkieszek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42  // Default buffer size if not defined
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+
+#endif
